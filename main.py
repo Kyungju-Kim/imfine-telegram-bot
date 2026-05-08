d710aba0d3bb55b5e3eca007856ddd144b49adb1
Kyungju.Kim <46181548+Kyungju-Kim@users.noreply.github.com>
2026-05-08 18:02:13 +0900

    Update main.py

--- main.py
+++ main.py
@@ -385,7 +385,7 @@ def main():
     scheduler.add_job(
         run_monitor,
         trigger="cron",
-        minute="0,3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,48,51,54,57",
+        minute="0,5,10,15,20,25,30,35,40,45,50,55",
         args=[app],
         id="schedule_monitor"
     )
